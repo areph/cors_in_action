d371cdf4de591f6a632eecdd18a798b6c569f2ec
areph <40balmung@gmail.com>
2021-07-06 16:10:21 +0900

    set api posts

--- server.js
+++ server.js
@@ -3,13 +3,25 @@
 const express = require("express");
 
 const HOST = "0.0.0.0";
-const PORT = 8080;
+const PORT = 9999;
+
+// Blog posts
+const POSTS = {
+  1: { post: "This is the first blog post." },
+  2: { post: "This is the second blog post." },
+  3: { post: "This is the third blog post." },
+};
 
 // App
 const app = express();
 
 // use static files
 app.use(express.static("public"));
+
+// API
+app.get("/api/posts", (req, res) => {
+  res.json(POSTS);
+});
 
 app.listen(PORT, () =>
   console.log(
